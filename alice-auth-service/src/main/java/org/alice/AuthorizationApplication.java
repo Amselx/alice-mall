991080ba57729e57e2a0fb332b89518b0065de25
amse <123>
2021-01-12 16:47:26 +0800

    Initial commit

---
+++ alice-auth-service/src/main/java/org/alice/AuthorizationApplication.java
@@ -0,0 +1,17 @@
+package org.alice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Amse
+ * @Date: 12/01/2021 16:39
+ * @Version: 1.0.0
+ */
+@SpringBootApplication
+public class AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationApplication.class, args);
+    }
+}
