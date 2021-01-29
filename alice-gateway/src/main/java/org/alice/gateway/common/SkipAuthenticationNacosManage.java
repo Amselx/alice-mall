9b928aeab1223cba8c72da1e6fd716ed74113ffe
amse <123>
2021-01-29 18:25:21 +0800

    web common

--- alice-gateway/src/main/java/org/alice/gateway/common/SkipAuthenticationNacosManage.java
+++ alice-gateway/src/main/java/org/alice/gateway/common/SkipAuthenticationNacosManage.java
@@ -5,18 +5,23 @@ import com.alibaba.nacos.api.config.listener.Listener;
 import com.alibaba.nacos.api.exception.NacosException;
 import com.google.common.collect.Sets;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.Executor;
+import java.util.stream.Collectors;
 
 /**
+ * The type Skip authentication nacos manage.
+ *
  * @author Amse
  * @version 1.0.0
- * @date 25/01/2021 15:57
+ * @date 25 /01/2021 15:57
  */
 @Slf4j
 @Component
@@ -31,8 +36,30 @@ public class SkipAuthenticationNacosManage implements InitializingBean {
     private static Set<String> shouldSkipUrl = new HashSet<>();
 
     @Override
-    public void afterPropertiesSet() throws Exception {
+    public void afterPropertiesSet() {
+        updateSkipUrl();
         shouldSkipUrlListener();
+
+    }
+
+    private void updateSkipUrl() {
+        String config;
+        try {
+            config = nacosConfigManager.getConfigService().getConfig(gatewayProperties.getSkipUriDataId(), gatewayProperties.getSkipUriGroup(), 200);
+            skipUrlHandler(config);
+        } catch (NacosException e) {
+            log.error("updateSkipUrl-error", e);
+        }
+    }
+
+    private void skipUrlHandler(String config) {
+        if (StringUtils.isBlank(config)) {
+            return;
+        }
+        String[] split = config.split(",");
+        Set<String> uri = Arrays.stream(split).map(StringUtils::trim).collect(Collectors.toSet());
+        log.info("自动更新URI配置...\r\n" + uri);
+        shouldSkipUrl = uri;
     }
 
     private void shouldSkipUrlListener() {
@@ -47,12 +74,11 @@ public class SkipAuthenticationNacosManage implements InitializingBean {
 
                 @Override
                 public void receiveConfigInfo(String configInfo) {
-                    log.info("自动更新配置...\r\n" + configInfo);
-                    shouldSkipUrl = Sets.newHashSet(configInfo.split(","));
+                    skipUrlHandler(configInfo);
                 }
             });
         } catch (NacosException e) {
-            log.error("nacos-addListener-error", e);
+            log.error("nacos-addListener-shouldSkipUrlListener-error", e);
         }
     }
 
---
+++ alice-gateway/src/main/java/org/alice/gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package org.alice.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author Amse
+ * @version 1.0.0
+ * @date 29/01/2021 17:16
+ */
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+}
